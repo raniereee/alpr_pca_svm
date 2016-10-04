bac44b8ae06f92759e4c27542cadd43dd6c972ea
Raniere Machado <raniere@dba.eng.br>
2016-10-04 19:59:09 -0300

    created main app

---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include <cv.h>
+#include <highgui.h>
+#include <cvaux.h>
+#include <ml.h>
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+string getFilename(string s) {
+
+    char sep = '/';
+    char sepExt='.';
+
+    #ifdef _WIN32
+        sep = '\\';
+    #endif
+
+    size_t i = s.rfind(sep, s.length( ));
+    if (i != string::npos) {
+        string fn= (s.substr(i+1, s.length( ) - i));
+        size_t j = fn.rfind(sepExt, fn.length( ));
+        if (i != string::npos) {
+            return fn.substr(0,j);
+        }else{
+            return fn;
+        }
+    }else{
+        return "";
+    }
+}
+
+
+int main ( int argc, char** argv )
+{
+    cout << "OpenCV Automatic Number Plate Recognition\n";
+    char* filename;
+    Mat input_image;
+
+    //Check if user specify image to process
+    if(argc >= 2 )
+    {
+        filename= argv[1];
+        //load image  in gray level
+        input_image=imread(filename,1);
+    }else{
+        printf("Use:\n\t%s image\n",argv[0]);
+        return 0;
+    }
+
+    string filename_whithoutExt = getFilename(filename);
+    cout << "working with file: "<< filename_whithoutExt << "\n";
+
+    /* TODO: Implement the plate detection module
+    //Detect posibles plate regions
+    DetectRegions detectRegions;
+    detectRegions.setFilename(filename_whithoutExt);
+    detectRegions.saveRegions=false;
+    detectRegions.showSteps=true;
+    vector<Plate> posible_regions= detectRegions.run( input_image );
+    */
+
+
+    //SVM for each plate region to get valid car plates
+    //Read file storage.
+    FileStorage fs;
+    fs.open("PCA4SVM.xml", FileStorage::READ);
+    Mat SVM_TrainingData;
+    Mat SVM_Classes;
+    fs["TrainingData"] >> SVM_TrainingData;
+    fs["classes"] >> SVM_Classes;
+    //Set SVM params
+    CvSVMParams SVM_params;
+    SVM_params.svm_type = CvSVM::C_SVC;
+    SVM_params.kernel_type = CvSVM::LINEAR; //CvSVM::LINEAR;
+    SVM_params.degree = 0;
+    SVM_params.gamma = 1;
+    SVM_params.coef0 = 0;
+    SVM_params.C = 1;
+    SVM_params.nu = 0;
+    SVM_params.p = 0;
+    SVM_params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 1000, 0.01);
+    //Train SVM
+    CvSVM svmClassifier(SVM_TrainingData, SVM_Classes, Mat(), Mat(), SVM_params);
+
+
+
+
+    return 0;
+}
